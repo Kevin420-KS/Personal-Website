8bd035985e1bdaae88ab3652aaf0d85fa5fbf395
Kevin420-KS <kevinsebastian42069@gmail.com>
2025-08-18 15:58:32 +0700

    Coba-coba

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("close");
-    sidebar.classList.toggle("open");
 }
-
-console.log("Website berhasil dimuat!");

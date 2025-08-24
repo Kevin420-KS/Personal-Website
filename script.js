89b5d398b1c859b75ee1ad444dccd9d1c746b845
Kevin420-KS <kevinsebastian42069@gmail.com>
2025-08-24 21:31:56 +0700

    Coba-coba

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("close");
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("close");
 }

53da2b261fd04e7b6d66d91c8f685ddececf6cf2
Kevin420-KS <kevinsebastian42069@gmail.com>
2025-08-24 22:09:54 +0700

    Coba-coba

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function loadPage(page) {
-  fetch(`${page}/content.html`)
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("content").innerHTML = html;
-    })
-    .catch(err => {
-      document.getElementById("content").innerHTML = "<p>Halaman tidak ditemukan.</p>";
-    });
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("close");
 }
-
-// default buka home
-window.onload = () => loadPage("home");
